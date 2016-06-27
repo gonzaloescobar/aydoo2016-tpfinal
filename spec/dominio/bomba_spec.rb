1badb7c1d14011e90e3f7d227fa335379d174742
Gonza <g.d.escobar88@gmail.com>
2016-06-27 07:05:53 -0300

    bomba permite choque con bomba y pierde 100 unidades

--- spec/dominio/bomba_spec.rb
+++ spec/dominio/bomba_spec.rb
@@ -14,24 +14,31 @@ describe 'Bomba' do
     expect(bomba.masa).to eq 100
   end
 
-  it 'deberia retornar 50 de masa cuando nave choque con asteroide masa 100' do
+  it 'deberia retornar 50 de masa cuando bomba choque con asteroide masa 100' do
     bomba = Bomba.new 200, 200
     nave = Nave.new 50, 100
     bomba.chocar_con(nave)
     expect(bomba.vida).to eq 100
   end
 
-  it 'deberia retornar 0 de vida cuando nave choque con asteroide' do
+  it 'deberia retornar 0 de vida cuando bomba choque con asteroide' do
     bomba = Bomba.new 200, 100
     asteroide = Asteroide.new 30, 100
     bomba.chocar_con(asteroide)
     expect(bomba.vida).to eq 0
   end
 
-  it 'deberia retornar 0 de vida cuando nave choque con asteroide' do
+  it 'deberia retornar 0 de vida cuando bomba choque con asteroide' do
     bomba = Bomba.new 200, 100
     misil = Misil.new 30, 100
     bomba.chocar_con(misil)
+    expect(bomba.vida).to eq 100
+  end
+
+  it 'deberia retornar 100 de vida cuando bomba choque con bomba' do
+    bomba = Bomba.new 200, 100
+    otra_bomba = Bomba.new 30, 100
+    bomba.chocar_con(otra_bomba)
     expect(bomba.vida).to eq 100
   end
 

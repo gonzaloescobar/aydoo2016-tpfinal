cecb985d0c54ac056f5d91e7f26888a37e476493
Gonza <g.d.escobar88@gmail.com>
2016-06-27 04:06:57 -0300

    Se crea bomba con masa

--- spec/dominio/bomba_spec.rb
+++ spec/dominio/bomba_spec.rb
@@ -11,4 +11,9 @@ describe 'Bomba' do
     expect(bomba.vida).to eq 100
   end
 
+  it 'deberia retornar 100 de masa cuando se crea con 100' do
+    bomba = Bomba.new 200, 100
+    expect(bomba.masa).to eq 100
+  end
+
 end
